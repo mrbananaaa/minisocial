2f428dc975c8071427b15b67d24ba4a14edc1167
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-02 21:15:09 +0700

    refactor: move handler wiring on composition root level

--- server/internal/post/module.go
+++ server/internal/post/module.go
@@ -3,17 +3,13 @@ package post
 import (
 	"log/slog"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/mrbananaaa/minisocial/internal/platform/validation"
-	"github.com/mrbananaaa/minisocial/internal/post/api"
 	"github.com/mrbananaaa/minisocial/internal/post/application"
 	"github.com/mrbananaaa/minisocial/internal/post/repositories/postgres"
 	"github.com/mrbananaaa/minisocial/internal/post/repositories/postgres/sqlc"
 )
 
 type Module struct {
-	app     *application.Application
-	handler *api.Handler
+	app *application.Application
 }
 
 func New(
@@ -21,20 +17,13 @@ func New(
 	logger *slog.Logger,
 ) *Module {
 	repo := postgres.New(db)
-	validator := validation.New()
 	app := application.New(repo)
-	handler := api.New(app, validator)
 
 	return &Module{
-		app:     app,
-		handler: handler,
+		app: app,
 	}
 }
 
 func (m *Module) Service() *application.Application {
 	return m.app
-}
-
-func (m *Module) RegisterRoutes(r chi.Router) {
-	m.handler.RegisterRoutes(r)
 }
