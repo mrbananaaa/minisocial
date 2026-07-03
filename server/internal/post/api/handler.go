f6d76e46b01e1c91e75dc0be3e3e76601c54154b
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-03 12:48:10 +0700

    fix: forget to passing createpost to Handler 😀

--- server/internal/post/api/handler.go
+++ server/internal/post/api/handler.go
@@ -25,8 +25,9 @@ func New(
 	validator *validation.Validator,
 ) *Handler {
 	return &Handler{
-		app:       app,
-		validator: validator,
+		app:        app,
+		createpost: createpost,
+		validator:  validator,
 	}
 }
 
