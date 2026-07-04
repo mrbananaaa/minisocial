338fdd457524c33442cebbbbf610d1527c677951
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-04 19:15:28 +0700

    feat: create post domain event

--- server/internal/post/domain/event.go
+++ server/internal/post/domain/event.go
@@ -2,14 +2,10 @@ package domain
 
 import "github.com/google/uuid"
 
-type Event interface {
-	isEvent()
-}
-
 type PostCreated struct {
 	PostID   uuid.UUID
 	AuthorID uuid.UUID
 	Title    string
 }
 
-func (PostCreated) isEvent() {}
+func (PostCreated) IsEvent() {}
