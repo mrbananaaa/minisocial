c6cba33f458d19fa675a323df7fad3e5510e3904
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:51:13 +0700

    refactor: change Event implementation

--- server/internal/platform/events/contracts.go
+++ server/internal/platform/events/contracts.go
@@ -1,9 +1,29 @@
 package events
 
+import (
+	"context"
+
+	"github.com/mrbananaaa/minisocial/internal/platform/messaging"
+)
+
 type Event interface {
-	IsEvent()
+	EventType() string
+}
+
+type EventMessage struct {
+	Topic   string
+	Payload []byte
 }
 
 type EventSource interface {
 	PullEvents() []Event
+}
+
+type EventHandler interface {
+	EventType() string
+	Handle(context.Context, EventMessage) error
+}
+
+type Consumer interface {
+	Subscribe(ctx context.Context, topic string) (messaging.SubscribtionPayload, error)
 }
