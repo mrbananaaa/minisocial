5d6b0fccea4fa82b201798e6a4f8a771d5fc7cda
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-08 00:24:50 +0700

    refactor: renaming struct

--- server/internal/user/consumer/user_created_logger.go
+++ server/internal/user/consumer/user_created_logger.go
@@ -10,31 +10,36 @@ import (
 	"github.com/mrbananaaa/minisocial/internal/user/domain"
 )
 
-type UserCreatedLoggerHandler struct {
+type LoggerHandler struct {
 	logger *slog.Logger
 }
 
-func NewUserCreatedLoggerHandler(log *slog.Logger) *UserCreatedLoggerHandler {
-	return &UserCreatedLoggerHandler{
+func NewLoggerHandler(log *slog.Logger) *LoggerHandler {
+	return &LoggerHandler{
 		logger: log,
 	}
 }
 
-func (h *UserCreatedLoggerHandler) EventType() string {
+func (h *LoggerHandler) EventType() string {
 	return string(domain.EvUserCreated)
 }
 
-func (h *UserCreatedLoggerHandler) Handle(ctx context.Context, evtMsg events.EventMessage) error {
+func (h *LoggerHandler) Handle(
+	ctx context.Context,
+	event events.EventContext,
+) error {
 	var payload domain.UserCreated
-	if err := json.Unmarshal(evtMsg.Payload, &payload); err != nil {
+	if err := json.Unmarshal(event.Envelope.Payload, &payload); err != nil {
 		return fmt.Errorf("failed to marshal event: %w", err)
 	}
 
-	h.logger.Info("new user created",
-		"topic", evtMsg.Topic,
-		"id", payload.UserID,
-		"email", payload.Email,
-	)
+	h.logger.
+		With("[CONSUMER]", "user.created_logger").
+		Info("new user created",
+			"topic", event.Envelope.Topic,
+			"id", payload.UserID,
+			"email", payload.Email,
+		)
 
 	return nil
 }
