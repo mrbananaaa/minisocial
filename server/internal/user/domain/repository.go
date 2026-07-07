a945b33157b855c5ea7d5cd8bca71b7b4cea6277
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:53:45 +0700

    refactor: change user create and update interface

--- server/internal/user/domain/repository.go
+++ server/internal/user/domain/repository.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Repository interface {
-	Create(ctx context.Context, user *User) (*User, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*User, error)
 	GetByEmail(ctx context.Context, email string) (*User, error)
 	GetByUsername(ctx context.Context, username string) (*User, error)
-	Update(ctx context.Context, user *User) (*User, error)
+	Create(ctx context.Context, user *User) error
+	Update(ctx context.Context, user *User) error
 	Delete(ctx context.Context, id uuid.UUID) error
 }
