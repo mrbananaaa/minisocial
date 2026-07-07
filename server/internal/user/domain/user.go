35eeb3b7ef1febaf063437f3e2f15cd38e4e06c7
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:55:35 +0700

    feat: add user created event on user creation and some user update functionality

--- server/internal/user/domain/user.go
+++ server/internal/user/domain/user.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/mrbananaaa/minisocial/internal/platform/events"
 )
 
 type User struct {
@@ -17,4 +18,68 @@ type User struct {
 	AvatarURL    string
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
+
+	events []events.Event
+}
+
+func (u *User) addEvent(event events.Event) {
+	u.events = append(u.events, event)
+}
+
+func (u *User) PullEvents() []events.Event {
+	events := u.events
+	u.events = nil
+
+	return events
+}
+
+type NewUserInput struct {
+	Email        string
+	Username     string
+	Name         string
+	PasswordHash string
+}
+
+func New(input NewUserInput) (*User, error) {
+	now := time.Now()
+	user := &User{
+		ID:           uuid.New(),
+		Email:        input.Email,
+		Username:     input.Username,
+		Name:         input.Name,
+		PasswordHash: input.PasswordHash,
+		CreatedAt:    now,
+		UpdatedAt:    now,
+	}
+
+	user.addEvent(UserCreated{
+		UserID: user.ID,
+		Email:  user.Email,
+	})
+
+	return user, nil
+}
+
+// TODO: validate input
+func (u *User) UpdateName(name string) error {
+	u.Name = name
+	u.UpdatedAt = time.Now()
+
+	return nil
+}
+
+// TODO: validate input
+func (u *User) UpdateBio(bio string) error {
+	u.Bio = bio
+	u.UpdatedAt = time.Now()
+
+	return nil
+}
+
+// TODO: validate input
+func (u *User) UpdateAvatarURL(avatarURL string) error {
+	u.AvatarURL = avatarURL
+	u.UpdatedAt = time.Now()
+
+	return nil
 }
