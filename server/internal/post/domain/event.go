2a23fca333fa73dd70c75463c9979c09e953ea47
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:52:42 +0700

    refactor: change the create post event implementation

--- server/internal/post/domain/event.go
+++ server/internal/post/domain/event.go
@@ -2,10 +2,18 @@ package domain
 
 import "github.com/google/uuid"
 
+type EventType string
+
+const (
+	EvPostCreated EventType = "post.created"
+)
+
 type PostCreated struct {
 	PostID   uuid.UUID
 	AuthorID uuid.UUID
 	Title    string
 }
 
-func (PostCreated) IsEvent() {}
+func (PostCreated) EventType() string {
+	return string(EvPostCreated)
+}
