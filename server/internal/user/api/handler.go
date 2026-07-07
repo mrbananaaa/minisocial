e13d0250213572017c6d8043376e025df1e6bf55
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:57:31 +0700

    feat: use create user workflow inside create user handler

--- server/internal/user/api/handler.go
+++ server/internal/user/api/handler.go
@@ -10,15 +10,18 @@ import (
 	"github.com/mrbananaaa/minisocial/internal/platform/httpx"
 	"github.com/mrbananaaa/minisocial/internal/platform/validation"
 	"github.com/mrbananaaa/minisocial/internal/user/application"
+	createuser "github.com/mrbananaaa/minisocial/internal/workflows/create_user"
 )
 
 type Handler struct {
-	app       *application.Application
-	validator *validation.Validator
+	app        *application.Application
+	createuser *createuser.Workflow
+	validator  *validation.Validator
 }
 
 func New(
 	app *application.Application,
+	createuser *createuser.Workflow,
 	validator *validation.Validator,
 ) *Handler {
 	return &Handler{
@@ -40,13 +43,27 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	out, err := h.app.CreateUser(r.Context(), toCreateUserInput(req))
+	user, err := h.createuser.Execute(r.Context(), createuser.Input{
+		Email:    req.Email,
+		Username: req.Username,
+		Name:     req.Name,
+		Password: req.Password,
+	})
 	if err != nil {
 		httpx.Error(w, http.StatusBadRequest, err.Error(), nil)
 		return
 	}
 
-	httpx.Res(w, http.StatusCreated, "user created", toUserResponse(out.User))
+	httpx.Res(w, http.StatusCreated, "user created", UserResponse{
+		ID:        user.ID,
+		Email:     user.Email,
+		Username:  user.Username,
+		Name:      user.Name,
+		Bio:       user.Bio,
+		AvatarURL: user.AvatarURL,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	})
 }
 
 func (h *Handler) GetUser(w http.ResponseWriter, r *http.Request) {
