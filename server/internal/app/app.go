ea4bfb8dc7d6dab775085aae8bda94484c103f30
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:58:22 +0700

    feat: implement new update

--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -11,22 +11,30 @@ import (
 
 	"github.com/mrbananaaa/minisocial/internal/platform/config"
 	"github.com/mrbananaaa/minisocial/internal/platform/db"
+	"github.com/mrbananaaa/minisocial/internal/platform/events"
 	"github.com/mrbananaaa/minisocial/internal/platform/logger"
-	"github.com/mrbananaaa/minisocial/internal/platform/nats"
+	"github.com/mrbananaaa/minisocial/internal/platform/messaging"
+	"github.com/mrbananaaa/minisocial/internal/platform/messaging/jetstream"
 	"github.com/mrbananaaa/minisocial/internal/platform/validation"
 	"github.com/mrbananaaa/minisocial/internal/post"
 	"github.com/mrbananaaa/minisocial/internal/user"
 	createpost "github.com/mrbananaaa/minisocial/internal/workflows/create_post"
+	createuser "github.com/mrbananaaa/minisocial/internal/workflows/create_user"
 
 	postapi "github.com/mrbananaaa/minisocial/internal/post/api"
+	postConsumer "github.com/mrbananaaa/minisocial/internal/post/consumer"
 	userapi "github.com/mrbananaaa/minisocial/internal/user/api"
+	userConsumer "github.com/mrbananaaa/minisocial/internal/user/consumer"
 )
 
 type App struct {
-	Log        *slog.Logger
-	DB         *db.Database
-	httpServer *http.Server
-	cfg        *config.Config
+	Log *slog.Logger
+	DB  *db.Database
+	cfg *config.Config
+
+	messageBroker messaging.Broker
+	eventWorker   *events.Worker
+	httpServer    *http.Server
 }
 
 func New(cfg *config.Config) (*App, error) {
@@ -40,16 +48,33 @@ func New(cfg *config.Config) (*App, error) {
 	}
 	dbPool := database.GetPool()
 
-	_, err = nats.New(cfg.NATS)
-	if err != nil {
+	messageBroker := jetstream.New(cfg.NATS.URL)
+	if err := messageBroker.Connect(ctx); err != nil {
 		return nil, err
 	}
+
+	dispatcher := events.NewDispatcher()
+
+	dispatcher.Register(
+		userConsumer.NewUserCreatedLoggerHandler(log),
+		postConsumer.NewPostCreatedLoggerHandler(log),
+	)
+
+	eventWorker := events.NewWorker(
+		messageBroker,
+		dispatcher,
+		log,
+	)
 
 	txManager := db.NewTxManager(dbPool)
 
 	userModule := user.New(dbPool)
 	postModule := post.New(dbPool)
 
+	createUserWorkflow := createuser.New(
+		userModule.Service(),
+		txManager,
+	)
 	createPostWorkflow := createpost.New(
 		userModule.Service(),
 		postModule.Service(),
@@ -58,7 +83,7 @@ func New(cfg *config.Config) (*App, error) {
 
 	validator := validation.New()
 
-	userHandler := userapi.New(userModule.Service(), validator)
+	userHandler := userapi.New(userModule.Service(), createUserWorkflow, validator)
 	posthandler := postapi.New(postModule.Service(), createPostWorkflow, validator)
 
 	r := NewRouter(Routes{
@@ -75,10 +100,12 @@ func New(cfg *config.Config) (*App, error) {
 	}
 
 	return &App{
-		Log:        log,
-		DB:         database,
-		httpServer: s,
-		cfg:        cfg,
+		Log:           log,
+		DB:            database,
+		cfg:           cfg,
+		messageBroker: messageBroker,
+		eventWorker:   eventWorker,
+		httpServer:    s,
 	}, nil
 }
 
@@ -86,6 +113,8 @@ func (a *App) Run(ctx context.Context) error {
 	a.Log.Info("App is up and running ✨",
 		"port", a.cfg.HTTP.Port,
 	)
+
+	go a.eventWorker.Run(ctx)
 
 	if err := a.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return err
@@ -101,6 +130,10 @@ func (a *App) Shutdown(ctx context.Context) error {
 	// append each cleaning methods error
 	// then return it with erros.Join
 	var errs []error
+
+	if err := a.messageBroker.Close(ctx); err != nil {
+		errs = append(errs, err)
+	}
 
 	if err := a.httpServer.Shutdown(ctx); err != nil {
 		errs = append(errs, err)
