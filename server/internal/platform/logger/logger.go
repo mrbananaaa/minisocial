18449a5deef4fd43e540d7c50baf525e9528d9bb
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-07 13:49:05 +0700

    feat: use pretty logger on dev

--- server/internal/platform/logger/logger.go
+++ server/internal/platform/logger/logger.go
@@ -4,15 +4,31 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/Marlliton/slogpretty"
 )
 
 func New(cfg *Config) *slog.Logger {
-	handler := slog.NewJSONHandler(
+	var handler slog.Handler
+
+	handler = slog.NewJSONHandler(
 		os.Stdout,
 		&slog.HandlerOptions{
 			Level: parseLevel(cfg.Level),
 		},
 	)
+
+	// TODO: refactor cfg
+	if os.Getenv("APP_ENV") == "development" {
+		handler = slogpretty.New(os.Stdout, &slogpretty.Options{
+			Level:      parseLevel(cfg.Level),
+			AddSource:  true,
+			Colorful:   true,
+			Multiline:  true,
+			TimeFormat: time.Kitchen,
+		})
+	}
 
 	return slog.New(handler)
 }
