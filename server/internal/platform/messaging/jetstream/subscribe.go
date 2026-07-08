02d89d613d0cdd1fb18c806c91ab353ed46c5649
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-07-08 10:45:51 +0700

    refactor: add max deliver retry

--- server/internal/platform/messaging/jetstream/subscribe.go
+++ server/internal/platform/messaging/jetstream/subscribe.go
@@ -56,6 +56,7 @@ func (b *JetStreamBroker) createConsumer(
 		Durable:       "EVENT_PROCESSOR_MINISOCIAL",
 		FilterSubject: b.withPrefix(topic),
 		AckPolicy:     jetstream.AckExplicitPolicy,
+		MaxDeliver:    10,
 	}
 
 	consumer, err := stream.CreateOrUpdateConsumer(
